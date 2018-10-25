14825b5e64792962c58125ee7fa5a1c33aa10792
art <m.arata9529@gmail.com>
2018-10-25 17:19:30 +0900

    edit routes.rb & add accounts haml
    マイページのルーティングを設定し、ビューを作成しました

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,10 @@ Rails.application.routes.draw do
   root 'titles#index'
   get 'titles' => 'titles#index'
   resources :users, only: [:edit, :update]
+
+  get '/account', to: 'accounts#edit'
+  get '/account/photo', to: 'accounts#photo'
+  get '/account/password', to: 'accounts#password'
+  get '/account/email', to: 'accounts#email'
+  get '/account/delete', to: 'accounts#delete'
 end
