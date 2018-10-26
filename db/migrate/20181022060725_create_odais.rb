b3ab7c8daa21e47534fd040f6ca06ce52293ba58
nakauchiayaka <ayakaaaaa61@gmail.com>
2018-10-26 16:19:36 +0900

    マイグレーションファイルをロールバック
    ロールバックして、odaiテーブルの
    Image,titleカラムにNOTNULL制約をつけた。

--- db/migrate/20181022060725_create_odais.rb
+++ db/migrate/20181022060725_create_odais.rb
@@ -1,8 +1,9 @@
 class CreateOdais < ActiveRecord::Migration[5.0]
   def change
     create_table :odais do |t|
-      t.text :title
-      t.text :image
+      t.text :title,null: false
+      t.text :image,null: false
+      t.references :user,null: false, foreign_key: true
       t.timestamps
     end
   end
--- db/migrate/20181026051939_add_user_id_to_odais.rb
+++
@@ -1,5 +0,0 @@
-class AddUserIdToOdais < ActiveRecord::Migration[5.0]
-  def change
-    add_column :odais, :user_id, :integer
-  end
-end
