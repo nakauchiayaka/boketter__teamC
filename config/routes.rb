5a4122ac37a9428c53e4fadaaac3d0c64f4082b3
art <m.arata9529@gmail.com>
2018-10-30 11:32:19 +0900

    edit routes.rb
    マイページ画面とミュート画面のルーティングを追加しました

--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,14 @@ Rails.application.routes.draw do
   devise_for :users
   root 'titles#index'
   get 'titles' => 'titles#index'
-  put '/users/password_upadate', to: 'users#password_update'
+  put '/users/password_upadate',  to: 'users#password_update'
   resources :users, only: [:edit, :update]
 
-  get '/account', to: 'accounts#edit'
-  get '/account/photo', to: 'accounts#photo'
-  get '/account/password', to: 'accounts#password'
-  get '/account/email', to: 'accounts#email'
-  get '/account/delete', to: 'accounts#delete'
+  get '/my',                to: 'accounts#my'
+  get '/account',           to: 'accounts#edit'
+  get '/account/photo',     to: 'accounts#photo'
+  get '/account/password',  to: 'accounts#password'
+  get '/account/email',     to: 'accounts#email'
+  get '/account/delete',    to: 'accounts#delete'
+  get '/mute',              to: 'accounts#mute'
 end
