af7798401b6bfb55dad86ac7b2bb67892eed4bb9
nakauchiayaka <ayakaaaaa61@gmail.com>
2018-11-01 11:04:50 +0900

    ボケ投稿機能修正
    _odai-list.html.haml部分テンプレートの該当箇所を修正

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 20181029085030) do
 
   create_table "bokes", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
