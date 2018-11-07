77e635570f939504ff86bca48826d5ceea015544
nakauchiayaka <ayakaaaaa61@gmail.com>
2018-11-07 10:07:52 +0900

    バツマークを押してdeleteする処理実装

--- app/assets/javascripts/star.js
+++ app/assets/javascripts/star.js
@@ -7,16 +7,16 @@ $(function() {
 
   function star_function(id, button) {
   if (button.hasClass("decrement")){
-      $.ajax({url: "/bokes/" + id + "/stars/" + id,
-              type: "delete",
-              data: {boke_id: id},
-              dataType: "json"
-            })
-    .done(function(data){
-      console.log("ttt")
-      button.removeClass("decrement").addClass("increment")
-      button.removeClass('fas fa-star').addClass('far fa-star');
-    });
+    //   $.ajax({url: "/bokes/" + id + "/stars/" + id,
+    //           type: "delete",
+    //           data: {boke_id: id},
+    //           dataType: "json"
+    //         })
+    // .done(function(data){
+    //   console.log("ttt")
+    //   button.removeClass("decrement").addClass("increment")
+    //   button.removeClass('fas fa-star').addClass('far fa-star');
+    // });
 
     }else{
       console.log("cccc");
@@ -27,35 +27,36 @@ $(function() {
             })
       .done(function(data){
       button.removeClass("increment").addClass("decrement")
-      button.empty();
-      button.addClass('fas fa-star');
+      button.removeClass('far fa-star').addClass('fas fa-star');
       })
     }
 }
 
 
-// $(".boke-rate-times").on("click",function(){
-//   id = $('.now-boke_id').val();
-//   star_delete_function(id,$(this));
+$(".boke-rate-times").on("click",function(){
+  var id = $(this).data('id');
+  console.log(id);
+  star_delete_function(id,$(this));
 
-// });
+});
 
-//   function star_delete_function(id,button){
-//   if (button.hasClass("decrement")){
+  function star_delete_function(id,button){
+  var star1 = button.parent().find('.star-1');
+  if (star1.hasClass("decrement")){
 
-//     $.ajax({url: "/bokes/" + id + "/stars/" + id,
-//               type: "delete",
-//               data: {boke_id: id},
-//               dataType: "json"
-//             })
-//     .done(function(data){
-//       console.log("ttt")
-//       button.removeClass("decrement").addClass("increment")
-//       button.empty();
-//       button.addClass('far fa-star');
-//     });
-//   }
-//   }
+    $.ajax({url: "/bokes/" + id + "/stars/" + id,
+              type: "delete",
+              data: {boke_id: id},
+              dataType: "json"
+            })
+    .done(function(data){
+      console.log("ttt")
+      star1.removeClass("decrement").addClass("increment")
+      star1.removeClass('fas fa-star').addClass('far fa-star');
+    });
+  }
+
+  }
 
 
 
