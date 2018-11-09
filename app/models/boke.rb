074a313d652254094266ca9cbf8eea9746925362
art <m.arata9529@gmail.com>
2018-11-09 13:03:11 +0900

    edit model
    commentモデルを作成しました
    bokeモデルにボケと一緒にコメントも消えるよう追記しました

--- app/models/boke.rb
+++ app/models/boke.rb
@@ -1,9 +1,8 @@
 class Boke < ApplicationRecord
   belongs_to :odai
   belongs_to :user
-  has_many :comments
+  has_many :comments, dependent: :destroy
   has_many :stars
   has_many :boke_tags
-  has_many :tags ,through: :boke_tags
-
+  has_many :tags, through: :boke_tags
 end
