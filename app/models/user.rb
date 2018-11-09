ab14565f542dd9a3c1b86da3cf467c57201b9edb
art <m.arata9529@gmail.com>
2018-11-09 13:04:03 +0900

    edit user model
    commentとのアソシエーションを記述しました

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,9 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  has_many :odais, dependent: :destroy
-  has_many :bokes, dependent: :destroy
+  has_many :odais,    dependent: :destroy
+  has_many :bokes,    dependent: :destroy
+  has_many :comments, dependent: :destroy
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   mount_uploader :image, ImageUploader
