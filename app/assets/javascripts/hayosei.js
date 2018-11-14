fb2068e8987834628e0c610083ac69c9b43f84b1
panpop <shikibi_s@yahoo.co.jp>
2018-11-14 13:29:12 +0900

    はよせいループ
    トリガー　blur()
    要素から外れた時。
    boke_textが選択されていない状態になったら発火

--- app/assets/javascripts/hayosei.js
+++ app/assets/javascripts/hayosei.js
@@ -15,47 +15,75 @@ $(function(){
 
       setTimeout(function(){
         $("#hayo").fadeIn()
-      },10000);
+      },1000);
       setTimeout(function(){
         $("#hayo").fadeOut()
-      },15000);
+      },1500);
 
       setTimeout(function(){
         document.getElementById('hayosei').innerHTML = 'ボケったー楽しんでる？';
         $("#hayo").fadeIn()
-      },20000);
+      },2000);
       setTimeout(function(){
         $("#hayo").fadeOut()
-      },25000);
+      },2500);
 
       setTimeout(function(){
         document.getElementById('hayosei').innerHTML = 'ねぇ、30秒たったよ';
         $("#hayo").fadeIn()
-      },30000);
+      },3000);
       setTimeout(function(){
         $("#hayo").fadeOut()
-      },35000);
+      },3500);
 
           setTimeout(function(){
         document.getElementById('hayosei').innerHTML = 'まだー？';
         $("#hayo").fadeIn()
-      },40000);
+      },4000);
       setTimeout(function(){
         $("#hayo").fadeOut()
-      },45000);
+      },4500);
 
       setTimeout(function(){
         document.getElementById('hayosei').innerHTML = '・・・';
         $("#hayo").fadeIn()
-      },50000);
+      },5000);
       setTimeout(function(){
         $("#hayo").fadeOut()
-      },55000);
+      },5500);
 
       setTimeout(function(){
         document.getElementById('hayosei').innerHTML = '<span id="oko">はよせい！！</span>';
         $("#hayo").fadeIn()
-      },60000);
+
+        if ($("#oko").length !==0){
+          console.log('あるよ')
+            $(document).on('click', '#boke_text', function(){
+              document.getElementById('hayosei').innerHTML = '<span id="ok">がんばー</span>';
+              $("#hayo").fadeOut(3000)
+            });
+        }
+      },6000);
+
+      $('#boke_text').blur(function(){
+          console.log('いるよ')
+            $(document).on('click', '#boke_text', function(){
+              document.getElementById('hayosei').innerHTML = '<span id="ok">はやくしてよぉ〜</span>';
+              $("#hayo").fadeOut(3000)
+            });
+            setTimeout(function(){
+              document.getElementById('hayosei').innerHTML = 'どう〜？';
+              $("#hayo").fadeIn()
+            },2000);
+            setTimeout(function(){
+              document.getElementById('hayosei').innerHTML = '傑作できちゃった？';
+              $("#hayo").fadeIn()
+            },4000);
+            setTimeout(function(){
+              document.getElementById('hayosei').innerHTML = '<span id="oko">はよせい！！</span>';
+              $("#hayo").fadeIn()
+            },6000);
+      });
     }
 });
 
